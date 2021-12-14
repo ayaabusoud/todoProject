c882e66f86685623135e5d1b772fb788cfeb4e1f
Jana-mousa <92536722+Jana-mousa@users.noreply.github.com>
2021-12-14 09:47:18 +0200

    Update home.js

--- js/home.js
+++ js/home.js
@@ -21,34 +21,54 @@ let count = 0 ;
 function addNewTodo(){
 
     let todo = document.getElementById("todo-input");
-    let todoList =document.getElementById("todo-items");
+    let todoUl =document.getElementById("todo-items");
 
-    if (todo.value.length > 0){
+    if (todo.value.length != 0){
     count++;
     let itemLeft = document.getElementById("left");
     itemLeft.innerHTML = count;
 
-    let todoli = document.createElement('li');
-    todoli.classList.add('todo-item');
+    let todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo-item');
+   
 
-    todoli.innerHTML='<div class="check"><div class="check-marks"><img src="../images/icon-check.svg">';
-    let todoText = document.createElement('span');
-    todoText.classList.add('text');
-    todoText.innerHTML = todo.value;
+    let check = document.createElement('div');
+    check.classList.add('check');
+
+    let checkMark = document.createElement('div');
+    checkMark.classList.add('check-marks');
+
+    let checkImg = document.createElement('img');
+    checkImg.src="../images/icon-check.svg";
+    checkImg.alt = "check icon";
+
+    let todoli = document.createElement('li');
+    todoli.innerHTML = todo.value;
     
     let crossDiv = document.createElement("div");
     crossDiv.classList.add('crossDiv');
-    crossDiv.innerHTML = '<img src="../images/icon-cross.svg" class="delete" />';
-    
-    todoList.appendChild(todoli);
-    todoli.appendChild(todoText);
-    todoli.appendChild(crossDiv);
+    let cross =  document.createElement("img");
+    cross.src = "../images/icon-cross.svg";
+    cross.alt = "cross icon";
+    cross.classList.add('delete');
+    todoUl.appendChild(todoDiv);
+    todoDiv.appendChild(check);
+    check.appendChild(checkMark);
+    checkMark.appendChild(checkImg);
+    todoDiv.appendChild(todoli);
+    todoDiv.appendChild(crossDiv);
+    crossDiv.appendChild(cross);
     }
 
     let checklist = document.querySelectorAll(".check-marks");
     for (let i = 0 ; i < checklist.length ; i ++){
         
         checklist[i].addEventListener("click" , checkTodo);
+    }
+    
+    let deleteTodo = document.querySelectorAll(".crossDiv");
+    for(let i = 0 ; i < deleteTodo.length ; i ++){
+        deleteTodo[i].addEventListener("click",deleteTodoItem);
     }
 
 }
@@ -66,69 +86,11 @@ function checkTodo(){
         itemLeft.innerHTML = count; 
 }
 
-
-var input = document.getElementById('userInput');
-var enterButton = document.getElementById('enter');
-var deleteButtons = document.getElementsByClassName("Remove");
-  for(var i = 0; i < deleteButtons.length; i++){
-    deleteButtons[i].addEventListener('click', deleteListItem, false);
-  }
-
-var completeButtons =
-  document.getElementsByClassName("Complete");
-    for(var i = 0; i < completeButtons.length; i++){
-      completeButtons[i].addEventListener('click', completeListItem, false);
-    }
-var ul = document.querySelector('ul');
-
-function inputLength(){
- return input.value.length;
-};
-
-function listLength(){
-  return item.length;
+function deleteTodoItem(){
+  this.perentElement.remove();
+  count--;
+  let itemleft = document.getElementById("left");
+  itemLeft.innerHTML=count;
 }
 
-function createListElement(){
-  var li = document.createElement('li');
-  li.appendChild(document.createTextNode(input.value));
-  //makes text node 
   
-  var clonedButton = 
-     document.getElementById("buttons").cloneNode(true);
-      clonedButton.childNodes[1].addEventListener('click', deleteListItem, false); 
-      clonedButton.childNodes[3].addEventListener('click', completeListItem, false); 
-  
-  li.appendChild(clonedButton);
-  ul.appendChild(li);
-  input.value = "";
-  
-}
-
-function deleteListItem(){
-  	alert("Item was deleted");
-    // li.classList.add("delete");
-}
-
-function completeListItem(){
-  alert("Item was completed");
-  li.classLi
-  
-}
-
-function addListAfterClick(){
-  if(inputLength() > 0){
-    createListElement();
-  }
-}
-// Hit enter key and create list
-function addAfterPress(event){
-  if(inputLength() > 0 && event.which === 13){
-    createListElement();
-  }
-}
-
-
-
-enterButton.addEventListener('click', addListAfterClick);
-input.addEventListener('keypress', addAfterPress);
